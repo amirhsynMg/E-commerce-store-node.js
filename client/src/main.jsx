c430af2d3080ba82342ad2befd46a0695e5331e7
unknown <amirmg31830@gmail.com>
2025-01-02 15:40:20 +0330

    initial

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import { BrowserRouter } from "react-router-dom" // for routing and diffrent pages
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+import { BrowserRouter } from "react-router-dom";
 
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</StrictMode>
+);
